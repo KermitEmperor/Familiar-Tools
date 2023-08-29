01a94aa80d6cf78ee05f036784e8274fcbb4b026
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-08-29 09:59:57 +0200

    god help me

--- src/main/java/com/kermitemperor/familiartools/tools/ToolBase.java
+++ src/main/java/com/kermitemperor/familiartools/tools/ToolBase.java
@@ -2,7 +2,7 @@ package com.kermitemperor.familiartools.tools;
 
 import com.kermitemperor.familiartools.FamiliarTools;
 import net.minecraft.core.NonNullList;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.*;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -12,10 +12,14 @@ import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ToolBase extends Item {
-    public static final String NBT_COLOR = "HeadColor";
+    public static final String NBT_COLOR_HEAD = "HeadColor";
+    public static final String NBT_COLOR_BASE = "BaseColor";
+    public static final String NBT_NAME = "LocalizedName";
+    public static final String NBT_TIER = "Tier";
     public ToolBase(Properties pProperties) {
         super(pProperties);
     }
@@ -24,25 +28,45 @@ public class ToolBase extends Item {
         if (group == FamiliarTools.FAMILIAR_TAB) {
             ItemStack stack = new ItemStack(this);
             CompoundTag nbt = stack.getOrCreateTag();
-            nbt.putInt("HeadColor", 0x385374);
+            CompoundTag namenbt = new CompoundTag();
+            namenbt.putString("Name", "{Name:[{\"text\":\"Nice\",\"italic\":false}]}");
+            nbt.put("display", namenbt);
+            nbt.putString(NBT_NAME, "testing");
+            nbt.putInt(NBT_TIER, 1);
+            nbt.putInt(NBT_COLOR_HEAD, 0x385374);
+            nbt.putInt(NBT_COLOR_BASE, 0xABC008);
             items.add(stack);
         }
     }
-    public static void setToolColor(ItemStack stack, int color) {
-        stack.getOrCreateTag().putInt(NBT_COLOR, color);
+
+    public static String getToolName(ItemStack stack) {
+        return stack.getOrCreateTag().getString(NBT_NAME);
+    }
+
+    public static int getToolTier(ItemStack stack) {
+        return stack.getOrCreateTag().getInt(NBT_TIER);
+    }
+
+    public static void setToolColor(ItemStack stack, int headcolor, int basecolor) {
+        CompoundTag nbt = stack.getOrCreateTag();
+        nbt.putInt(NBT_COLOR_HEAD, headcolor);
+        nbt.putInt(NBT_COLOR_HEAD, basecolor);
     }
     
     @Override
     public @NotNull ItemStack getDefaultInstance() {
         ItemStack stack = new ItemStack(this);
-        setToolColor(stack,0x385374);
+        setToolColor(stack,0x385374, 0xffffff);
         return stack;
     }
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
         CompoundTag tag = stack.getOrCreateTag();
-        if (tag.contains(NBT_COLOR)) {
-            tooltip.add(Component.nullToEmpty("#" + Integer.toHexString(tag.getInt(NBT_COLOR)).toUpperCase()));
+        if (tag.contains(NBT_COLOR_HEAD)) {
+            tooltip.add(Component.nullToEmpty("#" + Integer.toHexString(tag.getInt(NBT_COLOR_HEAD)).toUpperCase()));
+        }
+        if (tag.contains(NBT_COLOR_BASE)) {
+            tooltip.add(Component.nullToEmpty("#" + Integer.toHexString(tag.getInt(NBT_COLOR_BASE)).toUpperCase()));
         }
     }
 }
--- src/main/java/com/kermitemperor/familiartools/tools/util/ToolColour.java
+++ src/main/java/com/kermitemperor/familiartools/tools/util/ToolColour.java
@@ -4,17 +4,23 @@ import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import static com.kermitemperor.familiartools.tools.ToolBase.NBT_COLOR_BASE;
+import static com.kermitemperor.familiartools.tools.ToolBase.NBT_COLOR_HEAD;
+
 public class ToolColour implements ItemColor {
-    public static final String NBT_COLOR = "HeadColor";
-    public static int getToolColor(ItemStack stack) {
-        return stack.getOrCreateTag().getInt(NBT_COLOR);
+    public static int getToolHeadColor(ItemStack stack) {
+        return stack.getOrCreateTag().getInt(NBT_COLOR_HEAD);
+    }
+
+    public static int getToolBaseColor(ItemStack stack) {
+        return stack.getOrCreateTag().getInt(NBT_COLOR_BASE);
     }
     @Override
     public int getColor(@NotNull ItemStack stack, int layer) {
         if (layer == 0)
-            return Color.singleColor(0xffffff);
+            return Color.singleColor(getToolBaseColor(stack));
         if (layer == 1)
-            return Color.singleColor(getToolColor(stack));
-        return 0;
+            return Color.singleColor(getToolHeadColor(stack));
+        return Color.singleColor(0x0f0f0f);
     }
 }
