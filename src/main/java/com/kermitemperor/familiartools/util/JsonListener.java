719abfdaf746f86e8bfc73657fd90bfe622e1e0e
Emperor Kermit <74988196+KermitEmperor@users.noreply.github.com>
2023-08-31 11:28:37 +0200

    it works (https://i.imgur.com/oEur3bI.png), but i sill don't know what to do with it

--- src/main/java/com/kermitemperor/familiartools/util/JsonListener.java
+++ src/main/java/com/kermitemperor/familiartools/util/JsonListener.java
@@ -4,19 +4,23 @@ package com.kermitemperor.familiartools.util;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+import org.slf4j.Logger;
 
 import java.util.Map;
 
 public class JsonListener extends SimpleJsonResourceReloadListener {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
     public static final JsonListener instance = new JsonListener();
 
     public JsonListener() {
         super(GSON, "materials");
+        LOGGER.info("do you work?");
     }
 
 
